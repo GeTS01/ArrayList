385ec055a29d9921c28e91e3f2df4fbe665395e5
Profiteam <93377078+GTF707@users.noreply.github.com>
2023-01-28 13:29:56 +0300

    a

--- src/com/company/ArrayList.java
+++ src/com/company/ArrayList.java
@@ -1,49 +1,97 @@
 package com.company;
 
 import java.util.*;
+import java.util.Iterator;
 
 public class ArrayList<S> implements List<Student> {
-    int count = 0;
-    int capacity = 2;
+    private int count = 0;
+    private int capacity = 10;
     private Student[] studentStorage = new Student[capacity];
 
-
     public void print() {
-        for (var item : studentStorage) {
-            System.out.println(item.toString());
-        }
+        Iterator<Student> iterator = Arrays.stream(studentStorage).iterator();
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
     }
 
+    /**
+     * возвращает колличество элементо
+     */
     @Override
     public int size() {
         return count;
     }
 
+    /**
+     * Возыращает True если этот список не содержит элементов
+     */
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
     }
 
+    /**
+     * Проверяет наличие элемента в списке, возвращает boolean
+     */
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
+    /**
+     * перебор списока через итератор
+     */
     @Override
     public Iterator<Student> iterator() {
-        return null;
+        return new Iterator<Student>() {
+            int cursor = 0;
+            int lastRet = -1;
+
+            /**
+             *проверка наличия следующего элемента в списке
+             */
+            @Override
+            public boolean hasNext() {
+                return cursor < count;
+            }
+
+            /**
+             * Вставка следующего элемента
+             */
+            @Override
+            public Student next() {
+                int i = cursor;
+                if (i >= count)
+                    throw new NoSuchElementException();
+                if (i >= studentStorage.length)
+                    throw new ConcurrentModificationException();
+                cursor = i + 1;
+                return (Student) studentStorage[lastRet = i];
+            }
+        };
     }
 
+    /**
+     * Возвращает массив, содержащий все элементы в этом списке в правильной последовательности
+     */
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(studentStorage, count);
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < count)
+            return (T[]) Arrays.copyOf(studentStorage, count, a.getClass());
+        System.arraycopy(studentStorage, 0, a, 0, count);
+        if (a.length > count)
+            a[count] = null;
+        return a;
     }
 
+    /**
+     * Добавляет новый элемент в конец списка. Возвращает boolean
+     */
     @Override
     public boolean add(Student student) {
         if (count == capacity) {
@@ -54,8 +102,11 @@ public class ArrayList<S> implements List<Student> {
         return true;
     }
 
+    /**
+     * увеличение массива
+     */
     private void increaseStudentStorage() {
-        capacity = capacity + 1;
+        capacity = capacity + 5;
         Student[] newStudentStorage = new Student[capacity];
         for (int i = 0; i < studentStorage.length; i++) {
             newStudentStorage[i] = studentStorage[i];
@@ -63,87 +114,259 @@ public class ArrayList<S> implements List<Student> {
         studentStorage = newStudentStorage;
     }
 
+    /**
+     * удалене объекта из списка и сдвиг элементов влево
+     */
     @Override
     public boolean remove(Object o) {
-
-        return false;
+        if (o == null) {
+            throw new NullPointerException("Такого элемента нет");
+        }
+        for (int i = 0; i < studentStorage.length; i++) {
+            if (o.equals(studentStorage[i])) {
+                studentStorage[i] = null;
+                for (int j = i; j < studentStorage.length - 1; j++) {
+                    studentStorage[j] = studentStorage[j + 1];
+                }
+            }
+            count--;
+        }
+        return true;
     }
 
+    /**
+     * проверка наличия переданной коллекции в списке, возвращает boolean
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (var item : c) {
+            if (!contains(item))
+                return false;
+        }
+        return true;
     }
 
+    /**
+     * добавляет все элементы переданной колекции в списак и сдвигает вправо
+     */
     @Override
     public boolean addAll(Collection<? extends Student> c) {
-        for (var item:c) {   //итератор и итерейбл
+        for (var item : c) {
             add(item);
         }
-       return true;
+        return true;
     }
 
+    /**
+     * добавляет все элементы переданной колекции в списак начиная с индекса и сдвигает вправо
+     */
     @Override
     public boolean addAll(int index, Collection<? extends Student> c) {
-        return false;
+        for (var item : c) {
+            add(index, item);
+        }
+        return true;
     }
 
+    /**
+     * удаляет все элементы переданной коллекции
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        for (var item : c) {
+            remove(item);
+        }
+        return true;
     }
 
+    /**
+     * удаляет все элементы, кроме переданной коллекции
+     */
     @Override
     public boolean retainAll(Collection<?> c) {
+        for (int i = 0; i < count; i++) {
+            var item = get(i);
+            if (!c.contains(item))
+                remove(item);
+        }
         return false;
     }
 
+    /**
+     * Удаление всех элементов из списка
+     */
     @Override
     public void clear() {
-        studentStorage = null;
+        for (int i = 0; i < count; i++) {
+            studentStorage[i] = null;
+        }
+        count = 0;
     }
 
+    /**
+     * Возвращает элемент, который расположен в указанной позиции списка.
+     */
     @Override
     public Student get(int index) {
+        if (index < 0 || index > studentStorage.length) {
+            return null;
+        }
         return studentStorage[index];
     }
 
     @Override
     public Student set(int index, Student element) {
-        studentStorage[index] = element;
-        return element;
+        if (index < 0 || index > studentStorage.length) {
+            return null;
+        }
+        return studentStorage[index] = element;
     }
 
+    /**
+     * Добавляет элемент в позицию index. При добавлении происходит
+     * сдвиг всех элементов справа от указанного индекса на 1 позицию
+     */
     @Override
     public void add(int index, Student element) {
+        if (index < 0 || index > studentStorage.length) {
+            return;
+        }
+        for (int i = studentStorage.length - 1; i > index; i--) {
+            studentStorage[i] = studentStorage[i - 1];
+        }
+        studentStorage[index] = element;
     }
 
+    /**
+     * Удаление элемента в указанной позиции индекса. После удаления сдвигает все элементы влево
+     */
     @Override
     public Student remove(int index) {
-        Student copy = studentStorage[index];
-        studentStorage[index] = null;
-        return copy;
+        if (index < 0 || index > studentStorage.length) {
+            return null;
+        }
+        Student removedStudent = studentStorage[index];
+        for (int i = index; i < studentStorage.length - 1; i++) {
+            studentStorage[i] = studentStorage[i + 1];
+        }
+        count--;
+        return removedStudent;
     }
 
-//    @Override
-//    public Student remove(int index) {
-//        return studentStorage[index] = null;
-////        final var student = studentStorage[index];
-//    }
-
+    /**
+     * Возвращается индекс первого (совпадениея) элемента в списке.
+     * Если элемент не найден, также возвращает -1.
+     */
     @Override
     public int indexOf(Object o) {
-
-        return 0;
+        return indexOfRange(o, count);
     }
 
+    int indexOfRange(Object o, int end) {
+
+        if (o == null) {
+            for (int i = 0; i < end; i++) {
+                if (studentStorage[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < end; i++) {
+                if (o.equals(studentStorage[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Возвращается индекс последнего (совпадениея) элемента в списке.
+     * Если элемент не найден, также возвращает -1.
+     */
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        return lastIndexOfRange(o, count);
+    }
+
+    int lastIndexOfRange(Object o, int end) {
+        if (o == null) {
+            for (int i = end - 1; i >= 0; i--) {
+                if (studentStorage[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = end - 1; i >= 0; i--) {
+                if (o.equals(studentStorage[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public ListIterator<Student> listIterator() {
-        return null;
+        return new ListIterator<>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return studentStorage[index] != null;
+            }
+
+            @Override
+            public Student next() {
+                return studentStorage[index++];
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return studentStorage[index] != null;
+            }
+
+            @Override
+            public Student previous() {
+                return studentStorage[index--];
+            }
+
+            @Override
+            public int nextIndex() {
+                if (studentStorage[index++] == null)
+                    return count;
+                else {
+                    return index++;
+                }
+            }
+
+            @Override
+            public int previousIndex() {
+                if (studentStorage[index--] == null)
+                    return -1;
+                else {
+                    return index--;
+                }
+            }
+
+            @Override
+            public void remove() {
+                studentStorage[index] = null;
+                for (int i = index; i < studentStorage.length - 1; i++) {
+                    studentStorage[i] = studentStorage[i + 1];
+                }
+            }
+
+            @Override
+            public void set(Student student) {
+
+            }
+
+            @Override
+            public void add(Student student) {
+
+            }
+        };
     }
 
     @Override
@@ -155,4 +378,5 @@ public class ArrayList<S> implements List<Student> {
     public List<Student> subList(int fromIndex, int toIndex) {
         return null;
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,55 +1,91 @@
 package com.company;
+import java.util.*;
 
-import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     static ArrayList<Student> arrayList = new ArrayList<>();
-    static Student student;
 
     public static void main(String[] args) {
         menu();
     }
 
     public static void menu() {
-//        try catch
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Выберите действие \n" +
-                    "1)Добавить студента\n" +
-                    "2)Получить студента по индексу \n" +
-                    "3)Заменить одного студента на другого\n" +
-                    "4)Удалить студента\n" +
-                    "5)Показать всех студентов\n" +
-                    "6) Вставить готовых студентов в список\n" +
-                    "7) Очистить список студентов\n"+
-                    "8) Получить индекс студента");
-            int namber = scanner.nextInt();
-            if (namber == 1) {
+                    "1) Добавить студента\n" +
+                    "2) Получить студента по индексу \n" +
+                    "3) Заменить одного студента на другого\n" +
+                    "4) Удалить студента\n" +
+                    "5) Показать всех студентов\n" +
+                    "6) Вставить готовых студентов в конец списка\n" +
+                    "7) Проверить есть ли такой студент\n" +
+                    "8) Получить индекс студента c начало списка\n" +
+                    "9) Получить индекс студента c конца списка\n" +
+                    "10) Получить колличество студентов\n" +
+                    "11) Проверить заполнен ли список\n" +
+                    "12) Добавить студента по индексу\n" +
+                    "13) Итератор\n" +
+                    "14) Проверить есть ли такой список\n" +
+                    "15) Удалить несколько элементов коллекции\n" +
+                    "16) Удалить коллекцию\n" +
+                    "17) Вставить готовых студентов в любое место списка\n" +
+                    "18) Очистить список студентов");
+            System.out.println("");
+            int number = scanner.nextInt();
+            if (number == 1) {
                 addStudent();
-            } else if (namber == 2) {
+            } else if (number == 2) {
                 getStudent();
-            } else if (namber == 3) {
+            } else if (number == 3) {
                 setStudent();
-            } else if (namber == 4) {
+            } else if (number == 4) {
                 deleteAllStudents();
-            } else if (namber == 5) {
+            } else if (number == 5) {
                 arrayList.print();
-            } else if (namber == 6) {
-                addСollection();
-            } else if (namber == 7) {
-                clearTheList();
-            }else if (namber == 8) {
+            } else if (number == 6) {
+                addCollection();
+            } else if (number == 7) {
+                checkContains();
+            } else if (number == 8) {
                 studentNumber();
+            } else if (number == 9) {
+                studentNumberEnd();
+            } else if (number == 10) {
+                System.out.println(arrayList.size());
+            } else if (number == 11) {
+                System.out.println(arrayList.isEmpty());
+            } else if (number == 12) {
+                addStudentIndex();
+            } else if (number == 13) {
+                Iterator<Student> iter = arrayList.iterator();
+                while (iter.hasNext())
+                    System.out.println(iter.next());
+            } else if (number == 14) {
+                checkContainsCollection();
+            } else if (number == 15) {
+                studentRemoveAll();
+            } else if (number == 16) {
+                deleteСollection();
+            } else if (number == 17) {
+                addCollectionIndex();
+            } else if (number == 18) {
+                clearTheList();
+            } else if (number == 19) {
+                System.out.println(arrayList.isEmpty());
+            } else if (number == 20) {
+                System.out.println(arrayList.size());
             } else {
                 System.out.println("Такого варианта нет");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             menu();
         }
     }
 
-    public static void addStudent() {                           // Добавить студента
+    public static void addStudent() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя студента");
         String name = scanner.nextLine();
@@ -81,52 +117,47 @@ public class Main {
 
     public static void setStudent() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Создайте нового студента");
-        System.out.println("Введите имя студента");
-        String name = scanner.nextLine();
-        System.out.println("Введите возраст студента");
-        int age = scanner.nextInt();
-        System.out.println("Введите средний рейтинг");
-        int averageRating = scanner.nextInt();
-        student = new Student(name, age, averageRating);
+        Student student1 = new Student("Андрей", 20, 7);
+        ArrayList<Student> arrayListTest = new ArrayList<>();
+        arrayListTest.add(student1);
         System.out.println("Введите индекс студента, которого хотите заменить");
         int index = scanner.nextInt();
-        System.out.println(arrayList.set(index, student));
-
+        arrayList.set(index, student1);
     }
 
-    public static void deleteAllStudents() {                  // Удалить всех студентов студента
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите индекс студента, которого хотите удалить");
-        int number = scanner.nextInt();
-        arrayList.remove(number);
-        System.out.println("Студент успешно удален");
-    }
+    public static void deleteAllStudents() {
+        Scanner deleteSt = new Scanner(System.in);
+        System.out.println("Выберите вариант удаления студента\n 1) Как обьект \n 2) По индексу ");
+        int numberSt = deleteSt.nextInt();
+        if (numberSt == 2) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Выберите индекс студента, которого хотите удалить");
+            int number = scanner.nextInt();
+            arrayList.remove(number);
+            System.out.println("Студент успешно удален");
+        } else if (numberSt == 1) {
+            Student student1 = new Student("2", 2, 2);
+            System.out.println(student1);
+            arrayList.remove(student1);
 
-    public static void addСollection() {
-        java.util.ArrayList<Student> arrayList2 = new java.util.ArrayList<>();
-        Student student1 = new Student("Георгйи", 18, 10);
-        Student student2 = new Student("Джон", 20, 3);
-        arrayList2.add(student1);
-        arrayList2.add(student2);
-        arrayList.addAll(arrayList2);
-
-
+        } else {
+            System.out.println("Такого варианта нет");
+        }
     }
 
     public static void clearTheList() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Вы точно хотите удалить всесь список \n"+
+        System.out.println("Вы точно хотите удалить всесь список \n" +
                 "1)Да\n2)нет");
         int number = scanner.nextInt();
-        if (number == 1){
+        if (number == 1) {
             arrayList.clear();
-        }else {
+        } else {
             menu();
         }
     }
 
-    public static void studentNumber(){
+    public static void studentNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя студента");
         String name = scanner.nextLine();
@@ -135,22 +166,89 @@ public class Main {
         System.out.println("Введите средний рейтинг");
         int averageRating = scanner.nextInt();
         Student student = new Student(name, age, averageRating);
-        arrayList.indexOf(student);
+        System.out.println(arrayList.indexOf(student));
+    }
 
+    public static void studentNumberEnd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя студента");
+        String name = scanner.nextLine();
+        System.out.println("Введите индекс студента");
+        int age = scanner.nextInt();
+        System.out.println("Введите средний рейтинг");
+        int averageRating = scanner.nextInt();
+        Student student = new Student(name, age, averageRating);
+        System.out.println(arrayList.lastIndexOf(student));
+    }
+
+    public static void checkContains() {
+        Student student1 = new Student("2", 2, 2);
+        System.out.println(student1);
+        var res = arrayList.contains(student1);
+        System.out.println(res);
+    }
+
+    public static void checkContainsCollection() {
+        Student student1 = new Student("1", 1, 1);
+        Student student2 = new Student("2", 2, 2);
+        System.out.println(student1);
+        System.out.println(student2);
+        ArrayList<Student> testArray = new ArrayList<>();
+        testArray.add(student1);
+        testArray.add(student2);
+        var res = arrayList.containsAll(testArray);
+        System.out.println(res);
+    }
+
+    public static void addStudentIndex() {
+        Scanner scanner = new Scanner(System.in);
+        Student student1 = new Student("Леха", 19, 9);
+        System.out.println(" Выберите индекс в который хотите добавить студента");
+        int number = scanner.nextInt();
+        arrayList.add(number, student1);
+    }
+
+    public static void addCollectionIndex() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Student> newArrayList = new ArrayList<>();
+        Student student1 = new Student("Иван", 20, 10);
+        Student student2 = new Student("Паша", 21, 8);
+        newArrayList.add(student1);
+        newArrayList.add(student2);
+        System.out.println(" Выберите индекс в который хотите добавить студентов");
+        int number = scanner.nextInt();
+        arrayList.addAll(number, newArrayList);
+    }
+
+    public static void addCollection() {
+        ArrayList<Student> arrayList2 = new ArrayList<>();
+        Student student1 = new Student("Георгйи", 18, 10);
+        Student student2 = new Student("Джон", 20, 3);
+        arrayList2.add(student1);
+        arrayList2.add(student2);
+        arrayList.addAll(arrayList2);
+    }
+
+    public static void studentRemoveAll() {
+
+        Student student1 = new Student("1", 1, 1);
+        Student student2 = new Student("2", 2, 2);
+        System.out.println(student1);
+        System.out.println(student2);
+        ArrayList<Student> testArray = new ArrayList<>();
+        testArray.add(student1);
+        testArray.add(student2);
+        arrayList.retainAll(testArray);
+    }
+
+    public static void deleteСollection() {
+        Student st1 = new Student("1", 1, 1);
+        Student st2 = new Student("3", 3, 3);
+        ArrayList<Student> testAr = new ArrayList<>();
+        testAr.add(st1);
+        testAr.add(st2);
+        var res = arrayList.removeAll(testAr);
+        System.out.println(res);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
